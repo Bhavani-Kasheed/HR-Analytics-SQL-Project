40e5080f27ddaaf8d52f025574a4298bcdfc5e5b
Bhavani kasheed <bhavanikasheed@gmail.com>
2025-10-03 10:49:37 +0530

    hr database

---
+++ hr-schema-mysql.sql
@@ -0,0 +1,14 @@
+SELECT * FROM northwind.product
+where unitPrice > 30 
+ORDER BY unitPrice DESC LIMIT 5;
+select companyName, city from northwind.customer
+where city = 'London' or 'Berlin' AND fax is NOT NULL order by city ; 
+select * from northwind.customer
+where city = 'Paris';
+delete from customer where city = 'Paris';
+DELETE FROM Customer WHERE City = 'Paris';
+select * from northwind.customer
+limit 10 ; 
+
+
+
